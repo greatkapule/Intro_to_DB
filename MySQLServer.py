ba05b6e93482abc6ad913605b02dfb5c34663129
edwin kapule <greatkapule@gmail.com>
2025-12-15 00:08:09 +0300

    Final fixes for task description and exception handling

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,27 +1,25 @@
 import mysql.connector
 
-def connect_and_create_db():
-    try:
+def create_database():
+    try:     
         mydb = mysql.connector.connect(
             host="localhost",
             user="root",
             password="Kapule@1"
         )
-        
         cursor = mydb.cursor()
         
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        
         print("Database 'alx_book_store' created successfully!")
 
     except mysql.connector.Error:
         print("Error while connecting to MySQL")
-    
+        
     finally:
         if 'mydb' in locals() and mydb.is_connected():
             cursor.close()
             mydb.close()
 
 if __name__ == "__main__":
-    connect_and_create_db()
+    create_database()
     
